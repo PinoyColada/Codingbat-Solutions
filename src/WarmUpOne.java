e56ba9c2dd74c19758e104026527f7bdad2e087f
Ryan Hatamosa <pinoydol.colada@gmail.com>
2023-03-06 23:54:10 -0500

    solved nearHundred question

--- src/WarmUpOne.java
+++ src/WarmUpOne.java
@@ -77,6 +77,19 @@ public class WarmUpOne {
         return a == 10 || b == 10 || a + b == 10;
     }
 
+    /* Given an int n, return true if it is within 10 of 100 or 200.
+     Note: Math.abs(num) computes the absolute value of a number.
+
+     nearHundred(93) → true
+    nearHundred(90) → true
+    nearHundred(89) → false
+     */
+
+    public boolean nearHundred(int n) {
+        return ((n >= 90 && n <= 110) || (n >= 190 && n <= 210));
+    }
+
+
 
 
 
