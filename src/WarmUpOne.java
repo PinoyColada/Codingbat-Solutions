a99fc489383d7a065bbc2a8b03485ee1feb99902
Ryan Hatamosa <pinoydol.colada@gmail.com>
2023-03-09 23:50:25 -0500

    solved notString question

--- src/WarmUpOne.java
+++ src/WarmUpOne.java
@@ -102,6 +102,27 @@ public class WarmUpOne {
         return ((((a < 0 && b > 0) || (a > 0 && b < 0)) && negative == false) || ((a < 0 && b < 0) && negative == true));
     }
 
+    /* Given a string, return a new string where "not " has been added to the front.
+    However, if the string already begins with "not", return the string unchanged.
+    Note: use .equals() to compare 2 strings.
+
+    notString("candy") → "not candy"
+    notString("x") → "not x"
+    notString("not bad") → "not bad"
+     */
+
+    public static String notString(String str) {
+        String not = "not";
+        if (str.length() < 3 ){
+            return not + " " + str;
+        }
+        if (not.equals(str.substring(0,3))){
+            return str;
+        }
+        return not + " " + str;
+    }
+
+
 
 
 
