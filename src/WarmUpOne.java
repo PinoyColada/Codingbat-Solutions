dcb0b4de318fcc9ce64098d7e220850365ad9b13
Ryan Hatamosa <pinoydol.colada@gmail.com>
2023-03-11 14:55:23 -0500

    solved frontBack question

--- src/WarmUpOne.java
+++ src/WarmUpOne.java
@@ -136,6 +136,21 @@ public class WarmUpOne {
         return str.substring(0, n) + str.substring(n + 1, str.length());
     }
 
+    /* Given a string, return a new string where the first and last chars have been exchanged.
+
+    frontBack("code") → "eodc"
+    frontBack("a") → "a"
+    frontBack("ab") → "ba"
+     */
+
+    public static String frontBack(String str) {
+        if (str.length() <= 1){
+            return str;
+        }
+        return str.charAt(str.length()-1) + str.substring(1, str.length() - 1)
+                + str.charAt(0);
+    }
+
 
 
 
