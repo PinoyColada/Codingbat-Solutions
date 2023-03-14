a1beddbf8e1a94717477bb00f93d6b3f55d66b3c
Ryan Hatamosa <pinoydol.colada@gmail.com>
2023-03-13 23:42:10 -0400

    solved backAround question

--- src/WarmUpOne.java
+++ src/WarmUpOne.java
@@ -167,6 +167,20 @@ public class WarmUpOne {
         return str.substring(0, 3) + str.substring(0, 3) + str.substring(0, 3);
     }
 
+    /* Given a string, take the last char and return a new string with the last char
+     added at the front and back, so "cat" yields "tcatt".
+      The original string will be length 1 or more.
+
+
+    backAround("cat") → "tcatt"
+    backAround("Hello") → "oHelloo"
+    backAround("a") → "aaa"
+     */
+
+    public static String backAround(String str) {
+        return str.charAt(str.length()-1) + str + str.charAt(str.length()-1);
+    }
+
 
 
 
