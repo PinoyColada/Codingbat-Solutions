dce4915f8fd74ab3ba038cfd3de8b897462201f4
Ryan Hatamosa <pinoydol.colada@gmail.com>
2023-03-17 23:42:02 -0400

    solved or35 question

--- src/WarmUpOne.java
+++ src/WarmUpOne.java
@@ -181,6 +181,18 @@ public class WarmUpOne {
         return str.charAt(str.length()-1) + str + str.charAt(str.length()-1);
     }
 
+    /* Return true if the given non-negative number is a multiple of 3 or a multiple of 5.
+
+    or35(3) → true
+    or35(10) → true
+    or35(8) → false
+     */
+    public static boolean or35(int n) {
+        return n % 3 == 0 || n % 5 == 0;
+    }
+
+
+
 
 
 
