0593d9d85e729395bc38a68249c4a7195500330c
Ryan Hatamosa <pinoydol.colada@gmail.com>
2023-03-19 23:49:31 -0400

    solved front22 question

--- src/WarmUpOne.java
+++ src/WarmUpOne.java
@@ -191,6 +191,23 @@ public class WarmUpOne {
         return n % 3 == 0 || n % 5 == 0;
     }
 
+    /* Given a string, take the first 2 chars and return the string with the 2 chars
+     added at both the front and back, so "kitten" yields"kikittenki".
+      If the string length is less than 2, use whatever chars are there.
+
+
+    front22("kitten") → "kikittenki"
+    front22("Ha") → "HaHaHa"
+    front22("abc") → "ababcab"
+     */
+
+    public static String front22(String str) {
+        if (str.length() < 2){
+            return str + str + str;
+        }
+        return str.substring(0,2) + str + str.substring(0,2);
+    }
+
 
 
 
