2aa3dc2e75320550ec7bcdb244bef375a9ac0884
Ryan Hatamosa <pinoydol.colada@gmail.com>
2023-03-22 13:43:05 -0400

    solved loneTeen question

--- src/WarmUpOne.java
+++ src/WarmUpOne.java
@@ -256,6 +256,22 @@ public class WarmUpOne {
         return ((a >= 13 && a <= 19) || (b >= 13 && b <= 19) || (c >= 13 && c <= 19));
     }
 
+    /* We'll say that a number is "teen" if it is in the range 13..19 inclusive.
+     Given 2 int values, return true if one or the other is teen, but not both.
+
+    loneTeen(13, 99) → true
+    loneTeen(21, 19) → true
+    loneTeen(13, 13) → false
+     */
+
+    public static boolean loneTeen(int a, int b) {
+        if ((a >= 13 && a <= 19) && (b >= 13 && b <= 19)
+                || !(a >= 13 && a <= 19) && !(b >= 13 && b <= 19) ){
+            return false;
+        }
+        return true;
+    }
+
 
 
 
