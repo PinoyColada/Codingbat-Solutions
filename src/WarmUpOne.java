eda4c88714ad42752fd6352d42c5100c7d9f3455
Ryan Hatamosa <pinoydol.colada@gmail.com>
2023-03-26 15:01:02 -0400

    solved everyNth question, finished Warmup-1 section.

--- src/WarmUpOne.java
+++ src/WarmUpOne.java
@@ -445,6 +445,26 @@ public class WarmUpOne {
         }
     }
 
+    /*
+    Given a non-empty string and an int N, return the string made starting with char 0,
+    and then every Nth char of the string. So if N is 3, use char 0, 3, 6, ... and so on.
+    N is 1 or more.
+
+    everyNth("Miracle", 2) → "Mrce"
+    everyNth("abcdefg", 2) → "aceg"
+    everyNth("abcdefg", 3) → "adg"
+     */
+
+    public static String everyNth(String str, int n) {
+        String newWord = "";
+        for (int i = 0; i < str.length(); i++){
+            if (i % n == 0){
+                newWord += str.charAt(i);
+            }
+        }
+        return newWord;
+    }
+
 
 
 
