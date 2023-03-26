2c104b9e886627cd4ed60649149355e00e04549f
Ryan Hatamosa <pinoydol.colada@gmail.com>
2023-03-26 14:41:00 -0400

    solved stringE question

--- src/WarmUpOne.java
+++ src/WarmUpOne.java
@@ -394,6 +394,26 @@ public class WarmUpOne {
         return 0;
     }
 
+    /* Return true if the given string contains between 1 and 3 'e' chars.
+
+    stringE("Hello") → true
+    stringE("Heelle") → true
+    stringE("Heelele") → false
+     */
+
+    public static boolean stringE(String str) {
+        int count = 0;
+        for (int i = 0; i <= str.length() - 1; i++){
+            if (str.charAt(i) == 'e'){
+                count++;
+            }
+        }
+        if (count >= 1 && count <= 3){
+            return true;
+        }
+        return false;
+    }
+
 
 
 
