c5fb6a3f880c55807a647ac1627906440693f619
Ryan Hatamosa <pinoydol.colada@gmail.com>
2023-03-26 14:17:31 -0400

    solved close10 question

--- src/WarmUpOne.java
+++ src/WarmUpOne.java
@@ -340,6 +340,24 @@ public class WarmUpOne {
         return c;
     }
 
+    /* Given 2 int values, return whichever value is nearest to the value 10,
+     or return 0 in the event of a tie.
+
+    close10(8, 13) → 8
+    close10(13, 8) → 8
+    close10(13, 7) → 0
+
+     */
+
+    public static int close10(int a, int b) {
+        if (a == b || Math.abs(a - 10) == Math.abs(b - 10)){
+            return 0;
+        } else if (Math.abs(a - 10) <= Math.abs(b - 10)) {
+            return a;
+        }
+        return b;
+    }
+
 
 
 
