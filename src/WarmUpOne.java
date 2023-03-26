aa618eca168c8ba27f0f2dece3249a4b36232d1d
Ryan Hatamosa <pinoydol.colada@gmail.com>
2023-03-26 14:30:48 -0400

    solved max1020- question

--- src/WarmUpOne.java
+++ src/WarmUpOne.java
@@ -372,6 +372,29 @@ public class WarmUpOne {
     }
 
 
+    /* Given 2 positive int values, return the larger value that is in the range 10..20 inclusive,
+     or return 0 if neither is in that range.
+
+    max1020(11, 19) → 19
+    max1020(19, 11) → 19
+    max1020(11, 9) → 11
+     */
+
+    public static int max1020(int a, int b) {
+        if (((a <= 20 && a >= 10) && (b <= 20 && b >= 10))){
+            if (a > b){
+                return a;
+            }
+            return b;
+        } else if (a <= 20 && a >= 10)  {
+            return a;
+        } else if (b <= 20 && b >= 10) {
+            return b;
+        }
+        return 0;
+    }
+
+
 
 
 
