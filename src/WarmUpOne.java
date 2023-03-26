70548d525490a4acd682034f0114c7be48b60e21
Ryan Hatamosa <pinoydol.colada@gmail.com>
2023-03-26 14:23:19 -0400

    solved in3050 question

--- src/WarmUpOne.java
+++ src/WarmUpOne.java
@@ -358,6 +358,19 @@ public class WarmUpOne {
         return b;
     }
 
+    /* Given 2 int values, return true if they are both in the range 30..40 inclusive,
+     or they are both in the range 40..50 inclusive.
+
+    in3050(30, 31) → true
+    in3050(30, 41) → false
+    in3050(40, 50) → true
+     */
+
+    public static boolean in3050(int a, int b) {
+        return (((a <= 50 && a >= 40) && (b <= 50 && b >= 40)) ||
+                ((a <= 40 && a >= 30) && (b <= 40 && b >= 30)));
+    }
+
 
 
 
