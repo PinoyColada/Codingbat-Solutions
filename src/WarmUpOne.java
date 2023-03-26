7176b4f2268aa716ac10685760b29323dd577984
Ryan Hatamosa <pinoydol.colada@gmail.com>
2023-03-26 14:55:41 -0400

    solved endUp question

--- src/WarmUpOne.java
+++ src/WarmUpOne.java
@@ -427,6 +427,27 @@ public class WarmUpOne {
         return ((a % 10) == (b % 10));
     }
 
+    /*
+    Given a string, return a new string where the last 3 chars are now in upper case.
+    If the string has less than 3 chars, uppercase whatever is there.
+
+    endUp("Hello") → "HeLLO"
+    endUp("hi there") → "hi thERE"
+    endUp("hi") → "HI"
+
+     */
+    public static String endUp(String str) {
+        if (str.length() < 3){
+            return str.toUpperCase();
+        } else {
+            return str.substring(0, str.length() - 3)
+                    + str.substring(str.length() - 3, str.length()).toUpperCase();
+        }
+    }
+
+
+
+
 
 
 
