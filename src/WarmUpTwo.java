bd91b81ffb8f6f4c585aa170989a57ad603545ef
Ryan Hatamosa <pinoydol.colada@gmail.com>
2023-03-27 09:38:43 -0400

    finished frontTimes question

--- src/WarmUpTwo.java
+++ src/WarmUpTwo.java
@@ -14,4 +14,29 @@ public class WarmUpTwo {
         }
         return newWord;
     }
+
+    /*
+    Given a string and a non-negative int n, we'll say that the front of the string is
+    the first 3 chars, or whatever is there if the string is less than length 3.
+    Return n copies of the front;
+
+    frontTimes("Chocolate", 2) → "ChoCho"
+    frontTimes("Chocolate", 3) → "ChoChoCho"
+    frontTimes("Abc", 3) → "AbcAbcAbc"
+
+     */
+
+    public String frontTimes(String str, int n) {
+        String newWord = "";
+        if (str.length() < 3){
+            for (int i = 0; i < n; i++){
+                newWord += str;
+            }
+        } else {
+            for (int i = 0; i < n; i++){
+                newWord += str.substring(0,3);
+            }
+        }
+        return newWord;
+    }
 }
