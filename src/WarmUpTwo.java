5d3ccdcd3be8e0019d5a9ac69b400a6c06659090
Ryan Hatamosa <pinoydol.colada@gmail.com>
2023-03-27 10:44:52 -0400

    finished stringBits question

--- src/WarmUpTwo.java
+++ src/WarmUpTwo.java
@@ -86,4 +86,21 @@ public class WarmUpTwo {
         return false;
     }
 
+    /*
+    Given a string, return a new string made of every other char starting with the first,
+    so "Hello" yields "Hlo".
+
+    stringBits("Hello") → "Hlo"
+    stringBits("Hi") → "H"
+    stringBits("Heeololeo") → "Hello"
+     */
+
+    public String stringBits(String str) {
+        String newWord = "";
+        for (int i = 0; i < str.length(); i += 2){
+            newWord += str.charAt(i);
+        }
+        return newWord;
+    }
+
 }
