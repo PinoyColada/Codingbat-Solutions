ccc8505ff0f0a40b3fc22492e5b192776e536d34
Ryan Hatamosa <pinoydol.colada@gmail.com>
2023-03-27 09:49:50 -0400

    finished countXX question

--- src/WarmUpTwo.java
+++ src/WarmUpTwo.java
@@ -39,4 +39,28 @@ public class WarmUpTwo {
         }
         return newWord;
     }
+
+    /* Count the number of "xx" in the given string. We'll say that overlapping is allowed,
+     so "xxx" contains 2 "xx".
+
+    countXX("abcxx") → 1
+    countXX("xxx") → 2
+    countXX("xxxx") → 3
+
+     */
+
+    public int countXX(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++){
+            if (str.charAt(i) == 'x'){
+                if (i + 1 >= str.length()){
+                    continue;
+                } else if (str.charAt(i + 1) == 'x') {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
 }
