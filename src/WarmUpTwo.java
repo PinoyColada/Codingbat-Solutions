29cc2c4162c1ef3666f4bc0edb557da01aed5ece
Ryan Hatamosa <pinoydol.colada@gmail.com>
2023-04-02 11:25:00 -0400

    finished stringX question

--- src/WarmUpTwo.java
+++ src/WarmUpTwo.java
@@ -224,6 +224,24 @@ public class WarmUpTwo {
         return counter;
     }
 
+    /* Given a string, return a version where all the "x" have been removed.
+     Except an "x" at the very start or end should not be removed.
+
+    stringX("xxHxix") → "xHix"
+    stringX("abxxxcd") → "abcd"
+    stringX("xabxxxcdx") → "xabcdx"
+     */
+
+    public String stringX(String str) {
+        String result = "";
+        for (int i=0; i<str.length(); i++) {
+            if (!(i > 0 && i < (str.length()-1) && str.substring(i, i+1).equals("x"))) {
+                result = result + str.substring(i, i+1);
+            }
+        }
+        return result;
+    }
+
 
     /* Given a string, return a string made of the chars at indexes
      0,1, 4,5, 8,9 ... so "kittens" yields "kien".
