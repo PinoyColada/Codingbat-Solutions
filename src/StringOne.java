c7e60133a6c6df5836c4dc7e6879618102a420ef
Ryan Hatamosa <pinoydol.colada@gmail.com>
2023-04-04 11:09:35 -0400

    solved extraEnd problem

--- src/StringOne.java
+++ src/StringOne.java
@@ -55,6 +55,20 @@ public class StringOne {
         return out.substring(0,2) + word + out.substring(2,4);
     }
 
+    /*
+    Given a string, return a new string made of 3 copies of the last 2 chars
+    of the original string. The string length will be at least 2.
+
+    extraEnd("Hello") → "lololo"
+    extraEnd("ab") → "ababab"
+    extraEnd("Hi") → "HiHiHi"
+     */
+
+    public String extraEnd(String str) {
+        return str.substring(str.length()-2) + str.substring(str.length()-2) +
+                str.substring(str.length()-2);
+    }
+
 
 
 }
