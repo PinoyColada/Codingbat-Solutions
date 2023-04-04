ae5c0e381686e57968c7225629eddba6443a6434
Ryan Hatamosa <pinoydol.colada@gmail.com>
2023-04-04 11:13:48 -0400

    solved firstHalf problem

--- src/StringOne.java
+++ src/StringOne.java
@@ -86,6 +86,19 @@ public class StringOne {
         return str.substring(0,2);
     }
 
+    /*
+    Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+
+    firstHalf("WooHoo") → "Woo"
+    firstHalf("HelloThere") → "Hello"
+    firstHalf("abcdef") → "abc"
+     */
+    public String firstHalf(String str) {
+        return str.substring(0, str.length() / 2);
+    }
+
+
+
 
 
 
