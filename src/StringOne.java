1d1cd6d77c3de75dd0186964393eece6aa843cab
Ryan Hatamosa <pinoydol.colada@gmail.com>
2023-04-04 11:24:33 -0400

    solved left2 problem

--- src/StringOne.java
+++ src/StringOne.java
@@ -141,6 +141,20 @@ public class StringOne {
         return a.substring(1, a.length()) + b.substring(1, b.length());
     }
 
+    /*
+    Given a string, return a "rotated left 2" version where the first 2 chars are moved to the end.
+    The string length will be at least 2.
+
+    left2("Hello") → "lloHe"
+    left2("java") → "vaja"
+    left2("Hi") → "Hi"
+     */
+
+    public String left2(String str) {
+        return str.substring(2, str.length()) + str.substring(0,2);
+    }
+
+
 
 
 
