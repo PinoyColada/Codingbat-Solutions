67ddd595f7df2899edce5498e452884f2f4f7cc8
Ryan Hatamosa <pinoydol.colada@gmail.com>
2023-04-05 08:54:23 -0400

    solved endsLy problem

--- src/StringOne.java
+++ src/StringOne.java
@@ -213,6 +213,22 @@ public class StringOne {
         return str.substring(str.length()/2 - 1, str.length()/2 + 1);
     }
 
+    /*
+    Given a string, return true if it ends in "ly".
+
+    endsLy("oddly") → true
+    endsLy("y") → false
+    endsLy("oddy") → false
+     */
+
+    public boolean endsLy(String str) {
+        if (str.length() < 2){
+            return false;
+        }
+        return (str.charAt(str.length()-2) == 'l' && str.charAt(str.length()-1) == 'y');
+    }
+
+
 
 
 
