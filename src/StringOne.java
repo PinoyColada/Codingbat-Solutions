04d4b465122878e1fee0c33c7167710581f97f9a
Ryan Hatamosa <pinoydol.colada@gmail.com>
2023-04-07 17:51:39 -0400

    solved lastChars problem

--- src/StringOne.java
+++ src/StringOne.java
@@ -259,6 +259,29 @@ public class StringOne {
         return str.substring(0,2);
     }
 
+    /*
+    Given 2 strings, a and b, return a new string made of the first char of a and the last char of b,
+    so "yo" and "java" yields "ya". If either string is length 0, use '@' for its missing char.
+
+
+    lastChars("last", "chars") → "ls"
+    lastChars("yo", "java") → "ya"
+    lastChars("hi", "") → "h@"
+     */
+
+    public String lastChars(String a, String b) {
+        if (a.length() == 0 && b.length() == 0 ){
+            return "@@";
+        } else if (a.length() == 0){
+            return "@" + b.charAt(b.length() - 1);
+        } else if (b.length() == 0){
+            return a.charAt(0) + "@";
+        }
+
+        return a.charAt(0) + b.substring(b.length() - 1);
+    }
+
+
 
 
 
