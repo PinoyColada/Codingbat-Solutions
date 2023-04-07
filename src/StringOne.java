8888877e8d636ba65d4296836ab90b07c5aa7ee0
Ryan Hatamosa <pinoydol.colada@gmail.com>
2023-04-06 22:08:21 -0400

    solved atFirst problem

--- src/StringOne.java
+++ src/StringOne.java
@@ -241,6 +241,25 @@ public class StringOne {
         return (str.substring(0, n) + str.substring(str.length() - n));
     }
 
+    /*
+    Given a string, return a string length 2 made of its first 2 chars.
+    If the string length is less than 2, use '@' for the missing chars.
+
+    atFirst("hello") → "he"
+    atFirst("hi") → "hi"
+    atFirst("h") → "h@"
+
+     */
+    public String atFirst(String str) {
+        if (str.length() == 0){
+            return "@@";
+        } else if (str.length() == 1){
+            return str + "@";
+        }
+        return str.substring(0,2);
+    }
+
+
 
 
 
