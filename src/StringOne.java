6da3f70fe2adcb0269fbb5f275a3258bf0e892fa
Ryan Hatamosa <pinoydol.colada@gmail.com>
2023-04-08 09:38:35 -0400

    solved minCat problem

--- src/StringOne.java
+++ src/StringOne.java
@@ -352,6 +352,24 @@ public class StringOne {
                 (str.substring(0,2).equals(str.substring(str.length() - 2))));
     }
 
+    /*
+    Given two strings, append them together (known as "concatenation") and return the result. However, if the strings are different lengths, omit chars from the longer string so it is the same length as the shorter string. So "Hello" and "Hi" yield "loHi". The strings may be any length.
+
+    minCat("Hello", "Hi") → "loHi"
+    minCat("Hello", "java") → "ellojava"
+    minCat("java", "Hello") → "javaello"
+     */
+
+    public String minCat(String a, String b) {
+        if (a.length() > b.length()){
+            return a.substring(a.length() - b.length()) + b;
+        } else if (a.length() < b.length()){
+            return a + b.substring(b.length() - a.length());
+        }
+        return a + b;
+    }
+
+
 
 
 
