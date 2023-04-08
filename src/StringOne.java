31c64f8c4d9412e4f09e4aee9a5e3c76490a5705
Ryan Hatamosa <pinoydol.colada@gmail.com>
2023-04-08 09:13:55 -0400

    solved seeColor problem

--- src/StringOne.java
+++ src/StringOne.java
@@ -318,6 +318,27 @@ public class StringOne {
                 str.substring(str.length()-1) + str.substring(str.length()-2, str.length()-1);
     }
 
+    /*
+    Given a string, if the string begins with "red" or "blue" return that color string,
+    otherwise return the empty string.
+
+    seeColor("redxx") → "red"
+    seeColor("xxred") → ""
+    seeColor("blueTimes") → "blue"
+     */
+
+    public String seeColor(String str) {
+        if (str.length() < 3){
+            return "";
+        } else if (str.substring(0,3).equals("red")){
+            return "red";
+        } else if (str.length() > 3 && str.substring(0,4).equals("blue")){
+            return "blue";
+        }
+        return "";
+    }
+
+
 
 
 
