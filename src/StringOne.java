c2960df6c781e82b61c2a72a53d8de026aa84d8c
Ryan Hatamosa <pinoydol.colada@gmail.com>
2023-04-09 13:41:52 -0400

    solved deFront problem

--- src/StringOne.java
+++ src/StringOne.java
@@ -399,6 +399,26 @@ public class StringOne {
         return str;
     }
 
+    /*
+    Given a string, return a version without the first 2 chars. Except keep the first char if it is 'a' and keep the second char if it is 'b'. The string may be any length. Harder than it looks.
+
+    deFront("Hello") → "llo"
+    deFront("java") → "va"
+    deFront("away") → "aay"
+     */
+
+    public String deFront(String str) {
+        if(str.charAt(0) == 'a' && str.charAt(1) == 'b'){
+            return str;
+        } else if (str.charAt(0) == 'a'){
+            return str.charAt(0) + str.substring(2);
+        } else if (str.charAt(1) == 'b') {
+            return str.charAt(1) + str.substring(2);
+        }
+        return str.substring(2);
+    }
+
+
 
 
 
