550d894e814dfd369e89eb338e9a2ed7a006cac1
Ryan Hatamosa <pinoydol.colada@gmail.com>
2023-04-14 16:11:56 -0400

    solved specialEleven problem

--- src/LogicOne.java
+++ src/LogicOne.java
@@ -155,4 +155,18 @@ public class LogicOne {
     public boolean in1To10(int n, boolean outsideMode) {
         return (!outsideMode && (n >= 1 && n <= 10) || (outsideMode && (n <= 1 || n >= 10)));
     }
+
+    /*
+    We'll say a number is special if it is a multiple of 11 or if it is one more than a multiple of 11.
+    Return true if the given non-negative number is special. Use the % "mod" operator -- see Introduction to Mod
+
+    specialEleven(22) → true
+    specialEleven(23) → true
+    specialEleven(24) → false
+     */
+
+    public boolean specialEleven(int n) {
+        return (n % 11 == 0 || n % 11 == 1);
+    }
+
 }
