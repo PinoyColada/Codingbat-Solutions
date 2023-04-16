ead1c615bde047d791ea26d4eac027d954db1806
Ryan Hatamosa <pinoydol.colada@gmail.com>
2023-04-16 13:45:52 -0400

    solved answerCell problem

--- src/LogicOne.java
+++ src/LogicOne.java
@@ -236,6 +236,28 @@ public class LogicOne {
         return a + b;
     }
 
+    /*
+    Your cell phone rings. Return true if you should answer it.
+    Normally you answer, except in the morning you only answer if it is your mom calling.
+    In all cases, if you are asleep, you do not answer.
+
+    answerCell(false, false, false) → true
+    answerCell(false, false, true) → false
+    answerCell(true, false, false) → false
+     */
+
+    public boolean answerCell(boolean isMorning, boolean isMom, boolean isAsleep) {
+        if (isAsleep){
+            return false;
+        } else if (!isAsleep && isMorning && isMom){
+            return true;
+        } else if (isMorning){
+            return false;
+        }
+        return true;
+    }
+
+
 
 
 
