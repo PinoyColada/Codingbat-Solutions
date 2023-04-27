0f02636a34667242d6a72c180dd55810764a907a
Ryan Hatamosa <pinoydol.colada@gmail.com>
2023-04-27 15:53:28 -0400

    solved makeLast problem

--- src/ArrayOne.java
+++ src/ArrayOne.java
@@ -178,6 +178,23 @@ public class ArrayOne {
         return (nums[0] != 2 && nums[1] != 2 & nums[0] != 3 && nums[1] != 3);
     }
 
+    /*
+    Given an int array, return a new array with double the length where its last element
+    is the same as the original array, and all the other elements are 0.
+    The original array will be length 1 or more. Note: by default, a new int array contains all 0's.
+
+    makeLast([4, 5, 6]) → [0, 0, 0, 0, 0, 6]
+    makeLast([1, 2]) → [0, 0, 0, 2]
+    makeLast([3]) → [0, 3]
+     */
+
+    public int[] makeLast(int[] nums) {
+        int[] res = new int[nums.length * 2];
+        res[res.length - 1] = nums[nums.length - 1];
+        return res;
+    }
+
+
 
 
 
